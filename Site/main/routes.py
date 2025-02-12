ddbc42a5d2cee4ff83bf1016b8887f41d8156b62
YousefD3ja <yousafdaaja2004@gmail.com>
2025-02-12 11:31:44 -0800

    Fixed a little bug

--- Site/main/routes.py
+++ Site/main/routes.py
@@ -9,7 +9,6 @@ def index():
 
 @main.route("/home", methods=["GET", "POST"])
 def home():
-    users = User.query(User.username)
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=2)
-    return render_template("home.html", title='Home', posts=posts, users=users)
+    return render_template("home.html", title='Home', posts=posts)
